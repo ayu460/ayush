2dcd2a83cdc91de3b727034950d642fa18504c2e
shivamtandon200500 <57308704+shivamtandon200500@users.noreply.github.com>
2021-10-04 20:28:57 +0530

    factorial.java

---
+++ factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        int s=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter num1ber ");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            s=s*i;
+        }
+        System.out.println("Factorial is "+s);
+    }
+}
